8546c6a16f22ebb559a7699fc9449c7e690f8fba
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-07-22 05:32:45 +0100

    setup theme provider (light or dark theme)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-function App() {
+export const App: React.FC = (): JSX.Element => {
+  const [themeMode, setThemeMode] = useState<"light" | "dark">("dark");
+  const theme = createTheme({
+    palette: {
+      mode: themeMode
+    },
+    typography: {
+      fontSize: 14
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">Dummy</div>
+    </ThemeProvider>
   );
 }
 
