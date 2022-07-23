15fe17de58afe4a520a0048640d28acb346f378a
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-07-23 16:36:10 +0100

    add DataGrid to Blotter page with dummy data

--- src/features/blotter/Blotter.tsx
+++ src/features/blotter/Blotter.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useCallback, useState } from "react";
+import { DataGrid } from "../../components";
 
 
 export const Blotter: React.FC = (): JSX.Element => {
+  const [showNoRowsOverlay, setShowNoRowsOverlay] = useState<boolean>(false);
+  const rowClickHandler = useCallback(() => {
+    // row click handler
+  }, [])
   return (
     <div>
-      <Typography component="div" variant="h3">
-        Blotter
-      </Typography>
+      <DataGrid
+        gridData={[{name: "typescript", percent: "89%"}, {name: "golang", percent: "98%"}]}
+        colDefs={[{field: "name"}, {field: "percent"}]}
+        size={{height: 1000, width: "100%"}}
+        showNoRowsOverlay={showNoRowsOverlay}
+        rowClickHandler={rowClickHandler}
+      ></DataGrid>
     </div>
   )
 }
