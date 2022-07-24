6e4a09eaabf8aa76ff04250dec94c60e6a89f631
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-07-24 16:52:07 +0100

    dynamic gridData in blotter datagrid and apt coldefs

--- src/features/blotter/Blotter.tsx
+++ src/features/blotter/Blotter.tsx
@@ -1,17 +1,20 @@
 import React, { useCallback, useState } from "react";
 import { DataGrid } from "../../components";
+import { useGetTradesQuery } from "../../services";
+import { tradeBlotterColDef } from "./config";
 
 
 export const Blotter: React.FC = (): JSX.Element => {
   const [showNoRowsOverlay, setShowNoRowsOverlay] = useState<boolean>(false);
+  const { data } = useGetTradesQuery(undefined, { pollingInterval: 30000});
   const rowClickHandler = useCallback(() => {
     // row click handler
   }, [])
   return (
     <div>
       <DataGrid
-        gridData={[{name: "typescript", percent: "89%"}, {name: "golang", percent: "98%"}]}
-        colDefs={[{field: "name"}, {field: "percent"}]}
+        gridData={data || []}
+        colDefs={tradeBlotterColDef}
         size={{height: 1000, width: "100%"}}
         showNoRowsOverlay={showNoRowsOverlay}
         rowClickHandler={rowClickHandler}
