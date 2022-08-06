b653da284acbd4f02fc36ef448d1e91191d9b8c2
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-08-06 03:48:57 +0100

    include dashboardApi reducerPath and middleware in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { dashboardApi } from "../services";
 import { tradesApi } from "../services/tradesApi";
 
 export const store = configureStore({
   reducer: {
     [tradesApi.reducerPath]: tradesApi.reducer,
+    [dashboardApi.reducerPath]: dashboardApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     tradesApi.middleware,
+    dashboardApi.middleware,
   )
 })
 
