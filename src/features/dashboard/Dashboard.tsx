b52a64215bad3b3d28aea5a96a8976b054cae51b
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-08-06 03:56:59 +0100

    update Dashboard page with all Dashboard metrics

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import { TopTrades } from "./components/TopTrades";
+import { RejectedTrades } from "./components/RejectedTrades";
+import { TotalEndCashChart } from "./components/TotalEndCashChart";
+import { TotalTrades } from "./components/TotalTrades";
+import { TotalClients } from "./components/TotalClients";
 
 
 export const Dashboard: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <Typography component="div" variant="h3">
-        Dashboard
+    <Stack spacing={2} sx={{ paddingTop: 3, paddingLeft: 2 }}>
+      <Typography component="div" variant="h5">
+        Repo Dashboard
       </Typography>
-    </div>
+      <Stack direction="row" spacing={2} sx={{ padding: 2 }}>
+        <TotalEndCashChart />
+        <Stack direction="column" spacing={2} sx={{ width: "100%" }}>
+          <TotalTrades />
+          <TotalClients />
+        </Stack>
+      </Stack>
+      <Stack direction="row" spacing={2} sx={{ paddingRight: 2 }}>
+        <TopTrades />
+        <RejectedTrades />
+      </Stack>
+    </Stack>
   )
 }
