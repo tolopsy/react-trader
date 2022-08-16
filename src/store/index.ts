1bd12ffa73f6cfc34e1f10cb52baa41c11d39952
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-08-16 09:57:09 +0100

    add ticketApi reducerPath and middleware to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { dashboardApi } from "../services";
-import { tradesApi } from "../services/tradesApi";
+import { dashboardApi, tradesApi, ticketApi } from "../services";
 
 export const store = configureStore({
   reducer: {
     [tradesApi.reducerPath]: tradesApi.reducer,
     [dashboardApi.reducerPath]: dashboardApi.reducer,
+    [ticketApi.reducerPath]: ticketApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     tradesApi.middleware,
     dashboardApi.middleware,
+    ticketApi.middleware,
   )
 })
 
