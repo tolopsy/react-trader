807f63ea9aa031bdd350446f909fa05aaa7c082d
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-08-18 03:29:39 +0100

    add reducer for ticketDefaultSlice to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { dashboardApi, tradesApi, ticketApi } from "../services";
+import { ticketDefaultSlice } from "./reducers/ticketDefaultSlice";
 
 export const store = configureStore({
   reducer: {
     [tradesApi.reducerPath]: tradesApi.reducer,
     [dashboardApi.reducerPath]: dashboardApi.reducer,
     [ticketApi.reducerPath]: ticketApi.reducer,
+    ticketDefaultState: ticketDefaultSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     tradesApi.middleware,
