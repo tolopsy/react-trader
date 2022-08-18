9519d329b36981b5de5a5cb960c6b358e20aa221
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-08-18 03:35:40 +0100

    add RepoTypeDropDown and OpenOrFixed to ticket section

--- src/features/tradeticket/components/TicketSections.tsx
+++ src/features/tradeticket/components/TicketSections.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import { RepoFormField } from "../../../components";
+import { useAppSelector } from "../../../store/hooks";
+import { RepoTypeDropDown } from "./RepoTypeDropDown";
+import { OpenOrFixed } from "./OpenOrFixed";
 
 
 interface Props {
@@ -8,6 +11,7 @@ interface Props {
 }
 
 export const TicketSections: React.FC<Props> = ({ ticketId }): JSX.Element => {
+  const { trader } = useAppSelector((state) => state.ticketDefaultState);
   return (
     <Stack direction="column" spacing={2}>
       <RepoFormField
@@ -17,6 +21,15 @@ export const TicketSections: React.FC<Props> = ({ ticketId }): JSX.Element => {
         type="text"
         value={ticketId}
       />
+      <RepoFormField
+        disabled
+        name="traderId"
+        label="Trader Id"
+        type="text"
+        value={trader}
+      />
+      <RepoTypeDropDown />
+      <OpenOrFixed />
     </Stack>
   )
 }
